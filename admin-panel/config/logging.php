6a63d5ced555e03f966975f517c2c787a9a8585c
PalmShift <rafaelmontillana12@gmail.com>
2024-11-28 21:29:01 +0800

    htaccess

--- admin-panel/config/logging.php
+++ admin-panel/config/logging.php
@@ -27,7 +27,7 @@ return [
     |
     | This option controls the log channel that should be used to log warnings
     | regarding deprecated PHP and library features. This allows you to get
-    | your application ready for upcoming major versions of dependencies.
+    | your application rey for upcoming major versions of dependencies.
     |
     */
 
